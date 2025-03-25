18c7a8f5dd960bad9330dfe814ac626e9a811790
thinkgo <thinkgo@aliyun.com>
2025-03-25 17:23:34 +0800

    feat: support darwin

--- sqlcipher_flags.go
+++ sqlcipher_flags.go
@@ -22,5 +22,6 @@ package sqlite3
 // set operating specific sqlite flags
 #cgo linux CFLAGS: -DSQLITE_OS_UNIX=1
 #cgo windows CFLAGS: -DSQLITE_OS_WIN=1
+#cgo darwin CFLAGS: -DSQLITE_OS_UNIX=1
 */
 import "C"
